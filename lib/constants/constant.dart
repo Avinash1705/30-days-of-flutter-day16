7b42e63c2937a33a796936e72b2d9115edfd51a5
Avinash1705 <avinashrawat1705@gmail.com>
2023-03-10 12:52:33 +0530

    model class filter made

---
+++ lib/constants/constant.dart
@@ -0,0 +1,4 @@
+class MyConstants {
+  static String dummyDescription =
+      "A mobile phone is a portable telephone that can make and receive calls over a radio frequency link while the user is moving within a telephone service area, as opposed to a fixed-location phone";
+}
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,15 +1,21 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
       body: Center(
-        child: Text("Cart"),
+        child: const Text("Cart").text.center.xl2.make(),
+      ),
+      bottomNavigationBar: Row(
+
       ),
     );
   }
